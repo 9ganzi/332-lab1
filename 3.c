5c7f0f1c79dfe2828d0187d3605d47705b458561
Jaehong Cho <gutiday14@gmail.com>
2021-02-16 03:03:19 -0500

    XYZ added

--- 3.c
+++ 3.c
@@ -3,9 +3,15 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+
+// (a) Read the next 50 characters from ​source.txt,​ and among characters read, replace each
+// character ’5’ with character ‘A’’ and all characters are then written in ​destination.txt
+// (b) Write characters "XYZ" into file ​destination.txt
+// ​(c) Repeat the previous steps until the end of file ​source.txt​. The last read step may not have 50
+// characters.
+
 
 int main(int argc, char *argv[])
 {
@@ -13,14 +19,13 @@ int main(int argc, char *argv[])
     int fd2 = open("destination.txt", O_WRONLY);
 
     char buffer[2];
-    while (read(fd1, buffer, 1) != 0)
-    {
-        if (buffer[0] == '5')
-            buffer[0] = 'A';
+    char xyz[] = "XYZ";
+    
+    
+    while (read(fd1, buffer, 1))
         write(fd2, buffer, 1);
-    }
+    write(fd2, xyz, 3);
     close(fd1);
     close(fd2);
-
     return 0;
 }
